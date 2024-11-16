81f599db1e66001f4662c8c7bdd91706497b726a
Sofia Hernandez <hernandezsofia5g@gmail.com>
2024-11-15 18:43:20 -0600

    Fixed footer and home page listings

--- frontend/js/pages/home/carousel.js
+++ frontend/js/pages/home/carousel.js
@@ -6,22 +6,6 @@ $(document).ready(function() {
             $('#autoWidth').removeClass('cS-hidden');
         } 
     });  
-
-    $('#autoWidth2').lightSlider({
-        autoWidth:true,
-        loop:true,
-        onSliderLoad: function() {
-            $('#autoWidth2').removeClass('cS-hidden');
-        } 
-    });
-
-    $('#autoWidth3').lightSlider({
-        autoWidth:true,
-        loop:true,
-        onSliderLoad: function() {
-            $('#autoWidth3').removeClass('cS-hidden');
-        } 
-    });
   });
 
 
